ec35f4bded63b017ae326ccc8a1dfc8caaf79861
humwooter <72936761+humwooter@users.noreply.github.com>
2023-10-18 13:20:54 -0700

    Add files via upload

--- authentication demo/authentication demo/ContentView.swift
+++ authentication demo/authentication demo/ContentView.swift
@@ -19,23 +19,36 @@ struct ContentView: View {
     @State var password = ""
     @State var loginSuccess = false
     @State var loginFailed = false
+    @State var isLoading = false
+
     
     var body: some View {
         NavigationView {
             if (!loginSuccess) {
-                VStack {
+                VStack(spacing: 25) {
+                    Spacer()
                     TextField("Enter email", text: $email)
                     SecureField("Enter password", text: $password)
-                    Button(action: {
-                        login()
-                    }, label: {
-                        Text("Sign in")
-                            .padding()
-                    })
+                    ZStack {
+                           Button(action: {
+                               login()
+                           }, label: {
+                               Text("Sign in")
+                                   .padding(10)
+                                   .foregroundColor(.white)
+                                   .background(Color.cyan)
+                                   .cornerRadius(30)
+                           })
+                           if isLoading {
+                               ProgressView()
+                           }
+                       }
                     if loginFailed {
                         Text("Login attempt failed")
                             .foregroundColor(.red)
                     }
+                    Spacer()
+                    Spacer()
                     
                 }
                 .padding()
@@ -43,12 +56,13 @@ struct ContentView: View {
                 .navigationTitle("Demo")
             }
             else {
-                HomeView()
+                HomeView(email: $email)
             }
         }
     }
     
     func login() {
+        isLoading = true
         Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
             if let _ = error {
                 self.loginFailed = true
@@ -62,22 +76,30 @@ struct ContentView: View {
 struct CustomNavigationView: View {
     @State var title: String
     @State var content: String
+    @Binding var email: String
     @Environment(\.colorScheme) var colorScheme
 
     
     
     var body : some View {
         NavigationStack {
-            VStack {
+            Form {
+                if (title == "Settings") {
+                    Section(header: Text("Email")) {
+                        Text("\(email)")
+                    }
+                }
                 if let asset = NSDataAsset(name: content) {
                   let data = asset.data
-                    if (isGIF(data: data)) {
-                        AnimatedImageView(data: data)
-                            .scaledToFit()
-                    }
-                    else {
-                        Image(uiImage: UIImage(data: data)!)
-                            .scaledToFit()
+                    Section {
+                        if (isGIF(data: data)) {
+                            AnimatedImageView(data: data)
+                                .scaledToFit()
+                        }
+                        else {
+                            Image(uiImage: UIImage(data: data)!)
+                                .scaledToFit()
+                        }
                     }
                 }
             }.navigationTitle($title)
@@ -87,30 +109,31 @@ struct CustomNavigationView: View {
 
 
 struct HomeView: View {
+    @Binding var email: String
     var body: some View {
         TabView {
-            CustomNavigationView(title: "Home", content: "festive_twerk")
+            CustomNavigationView(title: "Home", content: "festive_twerk", email: $email)
                     .tabItem {
                     Label("Home", systemImage: "house.fill")
                 }
             
-            CustomNavigationView(title: "Tab 1", content: "gif_1")
+            CustomNavigationView(title: "Tab 1", content: "gif_1", email: $email)
                 .tabItem {
                     Label("Tab 1", systemImage: "star.fill")
                 }
             
-            CustomNavigationView(title: "Tab 2", content: "gif_2")
+            CustomNavigationView(title: "Tab 2", content: "gif_2", email: $email)
                 .tabItem {
                     Label("Tab 2", systemImage: "bell.fill")
                 }
             
-            CustomNavigationView(title: "Tab 3", content: "gif_3")
+            CustomNavigationView(title: "Settings", content: "gif_3", email: $email)
                 .tabItem {
                     Label("Settings", systemImage: "gear")
                 }
      
             
-        }.accentColor(.red)
+        }.accentColor(.cyan)
     }
 }
 
